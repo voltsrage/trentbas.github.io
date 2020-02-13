0a16d7387fbb20cec4a41ad2e1af91b99ae19431
Trendon Cato <electric5388@hotmail.com>
2020-02-13 15:58:20 +0800

    Changed the Portfolio Style
    Changed from CubePortfolio to Isotope since Cube was not working and Isotope works better.

--- js/script.js
+++ js/script.js
@@ -98,6 +98,41 @@ $(function(){
             }
         }
         
-    })  
+    })   
    
+});
+
+$(function(){
+    'use strict'
+
+    var $projects = $('.projects');
+
+    $projects.isotope({
+        itemSelector: '.item',
+        layoutMode: 'fitRows'
+    });
+
+    $('ul.filters > li').on('click',function(e){
+
+        e.preventDefault();
+
+        var filter = $(this).attr('data-filter');
+
+        $('ul.filters > li').removeClass('active');
+        $(this).addClass('active');
+
+        $projects.isotope({filter: filter});
+    });
+
+    $('.card').mouseenter(function(){
+
+        $(this).find('.card-overlay').css({'top':'-100%'})
+        $(this).find('.card-hover').css({'top':'0'})
+    });
+
+    $('.card').mouseleave(function(){
+
+        $(this).find('.card-overlay').css({'top':'0'})
+        $(this).find('.card-hover').css({'top':'100%'})
+    });
 });
